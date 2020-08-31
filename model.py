2423b57c789e30e54276ea0afff32a3a71549b8d
island <island@localhost.localdomain>
2020-09-01 03:50:19 +0530

    all files 01/09-4am

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    
+    def __init__(self):
+        super(Model, self).__init__()
+
+        # generic
+        self.flatten = nn.Flatten()
+        self.relu = nn.
+
+        # conv layers
+        
+        # linear layers
+        nn.Linear()
